645feac0b7a53fb96ff09ecdde4da415a6b406d8
Eduardo Neves <neves98@outlook.com>
2019-02-22 14:01:25 -0300

    new exercise

--- python/Estruturas de Decisão/exercise6.py
+++ python/Estruturas de Decisão/exercise6.py
@@ -1,1 +1,13 @@
 # Faça um Programa que leia três números e mostre o maior deles.
+numero1 = float(input("Digite numero um: "))
+numero2 = float(input("Digite numero doi: "))
+numero3 = float(input("Digite numero tres: "))
+
+maior = numero1
+
+if numero2 < numero1 and numero2 < numero3:
+    maior = numero2
+if numero3 < numero1 and numero3 < numero2:
+    maior = numero3
+
+print(maior)
